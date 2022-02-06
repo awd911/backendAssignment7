637ec7739e6d15ec19034e44dedd85d00ea47a1b
awd911 <36082238+awd911@users.noreply.github.com>
2022-02-06 12:44:52 -0500

    Update HomeController.java
    changed restController back to Controller

--- src/main/java/com/example/getmesocialservice/HomeController.java
+++ src/main/java/com/example/getmesocialservice/HomeController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
     @Autowired
